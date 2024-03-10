8a41fdb7e341d5383b62560e1fb1ac9833003f1d
Rohan_Islam <rohanislam3669@gmail.com>
2024-03-10 22:11:29 +0600

    up

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -8,8 +8,8 @@ import {
 const router = express.Router();
 
 router.get("/", getUsers);
+router.get("/admins", getAdminUsers);
 router.get("/:id", getUserById);
-router.get("admins", getAdminUsers);
 router.post("/create-user", createUser);
 
 export default router;
